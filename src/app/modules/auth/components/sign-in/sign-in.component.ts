67b0f2e7e269797798d83106d06913f9044c1432
Lena Portianko <olena.portianko@titan-t.com>
2025-02-16 21:51:57 +0200

    Create sign in page

--- src/app/modules/auth/components/sign-in/sign-in.component.ts
+++ src/app/modules/auth/components/sign-in/sign-in.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrl: './sign-in.component.scss',
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
+  public signInForm: FormGroup = new FormGroup({});
+
+  public showPassword: boolean = false;
+
+  public constructor(private router: Router) {}
+
+  public ngOnInit(): void {
+    this.signInForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  public signIn(): void {
+    if (this.signInForm.invalid) {
+      this.signInForm.markAllAsTouched();
+      return;
+    }
+
+    this.router.navigate(['/']);
+  }
+
+  public signUp(): void {
+    this.router.navigate(['/auth/signup']);
+  }
 }
