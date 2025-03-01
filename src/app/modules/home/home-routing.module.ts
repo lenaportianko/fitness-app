31c60fda22cf5aed5edbc9c8efb89652db5c4aeb
Lena Portianko <olena.portianko@titan-t.com>
2025-03-01 19:28:44 +0200

    Add home routes

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ActivityComponent } from "./components/activity/activity.component";
+import { NutritionComponent } from "./components/nutrition/nutrition.component";
 
 const homeRoutes: Routes = [
+    { path: '', redirectTo: 'activity', pathMatch: 'full' },
+    { path: 'activity', component: ActivityComponent },
+    { path: 'nutrition', component: NutritionComponent }
 ];
 
 @NgModule({
