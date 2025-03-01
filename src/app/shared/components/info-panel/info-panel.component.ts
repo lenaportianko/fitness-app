6038785d133161a9432f14f2a746bff4506cc2f8
Lena Portianko <olena.portianko@titan-t.com>
2025-03-01 14:34:22 +0200

    Open the user details dialog from info panel

--- src/app/shared/components/info-panel/info-panel.component.ts
+++ src/app/shared/components/info-panel/info-panel.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { User } from '../../../core/models/user.model';
 import { UserService } from '../../../core/services/user.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UserDetailsDialogComponent } from '../user-details-dialog/user-details-dialog.component';
 
 @Component({
   selector: 'app-info-panel',
@@ -22,12 +24,25 @@ export class InfoPanelComponent implements OnInit {
   };
 
   public constructor(
-    private userService: UserService
+    private userService: UserService,
+    private dialog: MatDialog
   ) { }
 
   public ngOnInit(): void {
     this.userService.getUser().subscribe((userData: User) => {
       this.user = userData;
+    });
+  }
+
+  public openUserDetailsDialog(): void {
+    const dialogRef = this.dialog.open(UserDetailsDialogComponent, {
+      data: this.user
+    });
+
+    dialogRef.afterClosed().subscribe((updatedUser: Partial<User>) => {
+      if (updatedUser) {
+        this.user = { ...this.user, ...updatedUser };
+      }
     });
   }
 
