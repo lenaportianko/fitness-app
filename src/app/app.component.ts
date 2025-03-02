2d6e436bd112b0139c368576835dd7aef1dd65f3
Lena Portianko <olena.portianko@titan-t.com>
2025-03-02 14:20:46 +0200

    Remove header from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   imports: [
-    CommonModule,
-    RouterOutlet,
-    HeaderComponent
+    RouterOutlet
   ]
 })
 export class AppComponent {
 
-  public showHeader: boolean = true
-
-  public constructor(private router: Router, private activatedRoute: ActivatedRoute) {
-    this.router.events.subscribe(() => {
-      this.activatedRoute.firstChild?.data.subscribe(data => {
-        this.showHeader = data['showHeader'] !== false;
-      });
-    });
-  }
 }
