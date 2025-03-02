28b401e332c6a2d71747b22ba2442f5f34c41268
Lena Portianko <olena.portianko@titan-t.com>
2025-03-02 14:22:13 +0200

    Remove home routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,17 @@
 import { Routes } from "@angular/router";
 import { AuthGuard } from "./core/guards/auth.guard";
+import { HomeComponent } from "./modules/home/home.component";
 
 export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () => {
             return import('./modules/auth/auth-routing.module').then(m => m.AuthRoutingModule)
-        },
-        data: { showHeader: false }
+        }
     },
     {
         path: '',
-        loadChildren: () => {
-            return import('./modules/home/home-routing.module').then(m => m.HomeRoutingModule)
-        },
-        canActivate: [AuthGuard],
-        data: { showHeader: true }
+        component: HomeComponent,
+        canActivate: [AuthGuard]
     }
 ];
