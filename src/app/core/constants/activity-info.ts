0f25598abcb38e1fdd85f5da07d4a47a127cd3e0
Lena Portianko <olena.portianko@titan-t.com>
2025-04-28 20:50:55 +0300

    Initial activityInfo data

---
+++ src/app/core/constants/activity-info.ts
@@ -0,0 +1,32 @@
+import { ActivityInfo } from "../models/activity-info.model";
+import { ActivityType } from "../models/activity-type.enum";
+
+export const activityInfo: ActivityInfo[] = [
+    {
+        activityType: ActivityType.Sleep,
+        actualValue: 0,
+        goalValue: 8,
+        maxValue: 10,
+        unitOfMeasure: 'h'
+    },
+    {
+        activityType: ActivityType.Exercise,
+        actualValue: 0,
+        goalValue: 60,
+        maxValue: 120,
+        unitOfMeasure: 'min'
+    },
+    {
+        activityType: ActivityType.Exercise,
+        actualValue: 0,
+        goalValue: 3000,
+        unitOfMeasure: 'kcal'
+    },
+    {
+        activityType: ActivityType.Steps,
+        actualValue: 0,
+        goalValue: 6000,
+        maxValue: 30000,
+        unitOfMeasure: 'steps'
+    }
+]
