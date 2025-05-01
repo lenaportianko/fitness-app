2a3b6ae5dc405dc5cde1c3d49ff89edd0d1fefe2
Lena Portianko <olena.portianko@titan-t.com>
2025-05-01 11:53:15 +0300

    Activity action, reducer and effect

---
+++ src/app/shared/store/activity.action.ts
@@ -0,0 +1,6 @@
+import { createAction, props } from "@ngrx/store";
+import { ActivityInfo } from "../../core/models/activity-info.model";
+
+export const load = createAction("[Activity] Load");
+export const set = createAction("[Activity] Set", props<{ data: Record<string, ActivityInfo[]> }>());
+export const reset = createAction("[Activity] Reset");
---
+++ src/app/shared/store/activity.reducer.ts
@@ -0,0 +1,11 @@
+import { createReducer, on } from "@ngrx/store";
+import { ActivityInfo } from "../../core/models/activity-info.model";
+import { reset, set } from "./activity.action";
+
+export const initialState: Record<string, ActivityInfo[]> = {};
+
+export const activityReducer = createReducer(
+    initialState,
+    on(set, (_, { data }) => ({ ...data })),
+    on(reset, () => ({}))
+);
