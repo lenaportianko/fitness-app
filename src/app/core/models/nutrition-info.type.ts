c6a5eca43186adfa91bd59337da39294b208cdc5
Lena Portianko <olena.portianko@titan-t.com>
2025-06-09 16:51:06 +0300

    Change type of property

--- src/app/core/models/nutrition-info.type.ts
+++ src/app/core/models/nutrition-info.type.ts
@@ -1,8 +1,9 @@
 import { CourceType } from "./cource-type.enum";
+import { MealInfo } from "./meal-info.type";
 
 export type NutritionInfo = {
     courceType: CourceType;
     recommendedСalorie: number;
     actualCalorie: number;
-    dishes: string[];
+    dishes: MealInfo[];
 }
